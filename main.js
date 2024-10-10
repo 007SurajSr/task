35e94ed3b2d3afeb078f6114bd2e404c8dc14374
Suraj Gupta <8700suraj@gmail.com>
2024-10-10 10:59:29 +0530

    tast1

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("one").addEventListener("click", function() {
+    this.innerHTML = "Logical";
+});
+document.getElementById("two").addEventListener("click", function() {
+    this.innerHTML = "Flexible";
+});
+document.getElementById("three").addEventListener("click", function() {
+    this.innerHTML = " Solitary";
+});
+document.getElementById("four").addEventListener("click", function() {
+    this.innerHTML = "Imaginative";
+});
+ 
+document.getElementById("circle3").addEventListener("click", function() {
+    this.innerHTML = "River";
+});
+ 
